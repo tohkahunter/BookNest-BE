c8437520b685a1a56c4c6b4cf88e60a800537bf8
Vũ Đức Anh <ducanhvu43@gmail.com>
2025-06-09 20:13:45 +0700

    Add CRUD Author

---
+++ BookNest/BookNest_Repositories/Repository/AuthorRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookNest_Repositories.Interface;
+using BookNest_Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNest_Repositories.Repository
+{
+    public class AuthorRepository : GenericRepository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(BookTracker7Context context) : base(context)
+        {
+        }
+        // You can add any specific methods for AuthorRepository here if needed
+        // For example, methods to find authors by name, etc.
+    }
+}
